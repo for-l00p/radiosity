8da0f854c8cc2b3fe2187d7f5af5b0daae026835
laura <lfl47@cornell.edu>
2018-04-30 16:08:25 -0400

    approx 50,000 pts generatble

--- RayShoot.cu
+++ RayShoot.cu
@@ -3,13 +3,15 @@
 #include <math.h>
 #include <stdlib.h>
 #include <curand.h>
+#include <cstdio>
 #include <time.h>
+#include <ctime>
 #include <curand_kernel.h>
 #include <cuda.h>
 #include "errorchecking.cu"
 
-#define PATCH_NUM 1
-#define SAMPLES 5
+#define PATCH_NUM 40
+#define SAMPLES 1024
 
 struct PerRayData_pathtrace
 {
@@ -44,36 +46,36 @@ faces[] : array containing all the faces struct
 __global__ void generate_ray_dir(curandState *rand1, curandState *rand2, int n, PatchData *faces, int num, optix::float3 *dir, optix::float3 *pts) {
 
 	int idx = threadIdx.x + blockDim.x*blockIdx.x;
-
+	int i = blockIdx.x;
 	int count = 0;
-
+/*
 	for (int i = 0; i < num; i++) {
-		while (count < n) {
-			float sin_theta = sqrt(curand_uniform(rand1 + idx));
-			float cos_theta = sqrt(1 - sin_theta * sin_theta);
-			float psi = 2 * 3.14159265359 * curand_uniform((rand1  + idx));
-			float a1 = sin_theta * cos(psi);
-			float b1 = sin_theta * sin(psi);
-			float c1 = cos_theta;
+		while (count < n) {*/
+	float sin_theta = sqrt(curand_uniform(rand1 + idx));
+	float cos_theta = sqrt(1 - sin_theta * sin_theta);
+	float psi = 2 * 3.14159265359 * curand_uniform((rand1  + idx));
+	float a1 = sin_theta * cos(psi);
+	float b1 = sin_theta * sin(psi);
+	float c1 = cos_theta;
 
 			
-			optix::float3 v1 = a1 * (faces[i].b - faces[i].a);
-			optix::float3 v2 = b1 * (faces[i].c - faces[i].a);
-			optix::float3 v3 = c1 * faces[i].norm;
+	optix::float3 v1 = a1 * (faces[i].b - faces[i].a);
+	optix::float3 v2 = b1 * (faces[i].c - faces[i].a);
+	optix::float3 v3 = c1 * faces[i].norm;
 			
-			float r2 = curand_uniform((rand2 + idx));
-			float r1 = curand_uniform((rand2  + idx));
+	float r2 = curand_uniform((rand2 + idx));
+	float r1 = curand_uniform((rand2  + idx));
 
-			optix::float3 pt = (1.0 - sqrt(r1))*faces[i].a + (sqrt(r1) * (1.0 - r2))*faces[i].b + (r2 * sqrt(r1)*faces[i].c); 
-			optix::float3 direction = v1 + v2 + v3;
-			dir[i*n + count] = direction; //optix::make_float3(0.5f, 0.5f, 2.5* count); //
-			int pos = i * n + count;
-			optix::float3 pt2 = pt;//optix::make_float3(0.5f, 0.5f, count);
-			pts[pos] = pt2;//
-			count++;
-		}
-	}
-	count = 0;
+	optix::float3 pt = (1.0 - sqrt(r1))*faces[i].a + (sqrt(r1) * (1.0 - r2))*faces[i].b + (r2 * sqrt(r1)*faces[i].c); 
+	optix::float3 direction = v1 + v2 + v3;
+	dir[idx] = direction; //optix::make_float3(0.5f, 0.5f, 2.5* idx); //
+	//int pos = i * n + count;
+	//optix::float3 pt2 = optix::make_float3(0.5f, 0.5f, idx);
+	pts[idx] = pt;//
+	//		count++;
+	//	}
+	//}
+	//count = 0;
 }
 
 int main() {
@@ -93,12 +95,15 @@ int main() {
 	//host data structures
 	PatchData test;
 	PatchData *patches = (PatchData*)malloc(PATCH_NUM * sizeof(PatchData));
-	PatchData *t = (PatchData*)malloc(sizeof(PatchData));
-	t->a = optix::make_float3(1.0f, 1.0f, 1.0f);
-	t->b = optix::make_float3(1.0f, 0.0f, 1.0f);
-	t->c = optix::make_float3(1.0f, 1.0f, 0.0f);
-	t->norm = optix::make_float3(0.0f,-1.0f,0.0f);
-	patches[0] = *t;
+	int p = 0;
+	for (p = 0; p < PATCH_NUM; p++) {
+		PatchData *t = (PatchData*)malloc(sizeof(PatchData));
+		t->a = optix::make_float3(1.0f, 1.0f, 1.0f);
+		t->b = optix::make_float3(1.0f, 0.0f, 1.0f);
+		t->c = optix::make_float3(1.0f, 1.0f, 0.0f);
+		t->norm = optix::make_float3(0.0f, -1.0f, 0.0f);
+		patches[p] = *t;	
+	}
 
 	optix::float3 *c_dir_arr = (optix::float3*)malloc(SAMPLES *PATCH_NUM * sizeof(optix::float3));
 	optix::float3 *c_pt_arr = (optix::float3*)malloc(SAMPLES*PATCH_NUM * sizeof(optix::float3));
@@ -119,26 +124,27 @@ int main() {
 	CudaCheckError();
 
 	//dim3 grid(1, 1, 1);
-	//dim3 threads(num_threads, 1, 1);
-	//dim3 threads2(len, 1, 1); // more threads needed fir separate int2 version
-	//						  // execute the kernel
-	//kernel << < grid, threads >> >((int *)d_data);
+	//dim3 threads((PATCH_NUM + 255) / 256, 1, 1);
+	//int threads = (PATCH_NUM+63) / 64;
+	std::clock_t start;
+	float duration;
 
-	dim3 grid(1, 1, 1);
-	dim3 threads((PATCH_NUM + 63) / 64, 1, 1);
+	start = std::clock();
 
-	generate_ray_dir <<<grid, threads>>> (d_state, d_state1, SAMPLES, g_patch_arr, PATCH_NUM, g_dir_arr, g_pt_arr);
+	generate_ray_dir <<<PATCH_NUM, 1024>>> (d_state, d_state1, SAMPLES, g_patch_arr, PATCH_NUM, g_dir_arr, g_pt_arr);
 	CudaCheckError();
 	cudaMemcpy(c_dir_arr, g_dir_arr, SAMPLES*PATCH_NUM * sizeof(optix::float3), cudaMemcpyDeviceToHost);
 	cudaMemcpy(c_pt_arr, g_pt_arr, SAMPLES*PATCH_NUM * sizeof(optix::float3), cudaMemcpyDeviceToHost);
-	for (int i = 0; i < PATCH_NUM; i++) {
-		for (int j = 0; j < SAMPLES; j++) {
-			float f = c_pt_arr[i*SAMPLES + j].z;
-			float f2 = c_dir_arr[i*SAMPLES + j].z;
-			printf("%f , %f \n", f, f2);
-		}
+	duration = (std::clock() - start) / (float)CLOCKS_PER_SEC;
+	printf("%f\n", duration);
+	//for (int i = 0; i < PATCH_NUM; i++) {
+	//	for (int j = 0; j < SAMPLES; j++) {
+	float f = c_pt_arr[(PATCH_NUM - 1)*SAMPLES + SAMPLES-1].z;
+	float f2 = c_dir_arr[(PATCH_NUM - 1)*SAMPLES + SAMPLES - 1].z;
+	printf("%f , %f \n", f, f2);
+	/*	}
 		printf("\n");
-	}
+	}*/
 	printf("Done");
 	
 	free(patches);
